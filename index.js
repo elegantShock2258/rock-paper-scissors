c80faddcb6400e3eece73be7b5dfc8c2c52e3493
Ayush Chadha <elegantShock3307@gmail.com>
2022-10-28 03:04:45 +0530

    basic game functionality is done

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ let options = ["Rock 🪨", "Paper 📝", "Scissors ✂️"]
 let textarea = document.getElementById('selection')
 let statusElement = document.getElementById("status")
 let isDrawer = false;
-let isMouseIn = false;
 const typingThreshold = 10000
 
 
 let typingTimer = setTimeout(removeSelectionDrawer, typingThreshold);
-// update the status bar during startup
-// update status bar when left idle
 let statuses = ["Dont Be shy!!!", "Oh come on, you didn't start this to stare at the screen did you?"]
 
 
 let parent = document.getElementById("selectionContainer");
 let dropdownContainer = document.createElement("div");
+
+//TODO: add a sepereate button
+//      also add a section to show the computer's choice
+//      what if when u win the screen fades and there's an animation for the two choices coming togeather??????? ( what if u win in a certain streak??)
+//      add score mechanism
+//      add highscore mechanism
+
 
 function addSelectionDrawer() {
     parent = document.getElementById("selectionContainer");
@@ -39,61 +43,38 @@ function addSelectionDrawer() {
         dropdownContainer.appendChild(dropdownElement);
     }
 
-    dropdownContainer.addEventListener("mouseenter", (event) => {
-        console.log("mouseenter")
-        isMouseIn = true
-        clearTimeout(typingTimer);
-    })
-    dropdownContainer.addEventListener("mouseout", (event) => {
-        console.log("mouseout")    
-        isMouseIn = false
-        typingTimer = setTimeout(removeSelectionDrawer, typingThreshold);
-
-    })  
-    dropdownContainer.style.animation = "fadeIn ease 1.4s"
+    animationInitialise(dropdownContainer,"fadeIn ease 1.4s")
     parent.appendChild(dropdownContainer);
     isDrawer = true;
 }
 
 function startUp() {
+    textarea.readOnly = true;
     statusElement.textContent = statuses[randomNumber % statuses.length]
 
+    textarea.addEventListener("click", (event) => {
+        if(isDrawer) { 
+            removeSelectionDrawer()
+            isDrawer = false
+        } else {
+            addSelectionDrawer()
+            isDrawer = true;
+        }
+    })
 
-    //handle user input
     textarea.addEventListener("keydown", (event) => {
-        if (event.keyCode >= 65 && event.keyCode <= 90) {  // check if it is alphanumeric, without the numeric
-            let str = textarea.value.trim();
-            //add css class to indicate we're typing ?? like a smooth underline animation??
-            // let elementToHighlight = resolveElement(str);
-            // console.log(elementToHighlight.textContent);
-            // open/add the selection drawer (if it is already then dont!!)
-            if (!isDrawer) addSelectionDrawer()
-            else console.log("drawer already exists");
-            // highlight the selection
-            // elementToHighlight.classList.add("highlighted")
-            // elementToHighlight.addEventListener("onlcik", (e) => {
-                // textarea.textContent = elementToHighlight.textContent
-                // runGame()
-            // })
-        } else if(event.keyCode=== 13){
-            event.preventDefault()
+        if(event.keyCode === 13) {
+            event.preventDefault();
             runGame()
         }
     })
-    textarea.addEventListener("keyup", (event) => {
-        if(isMouseIn) clearTimeout(typingTimer) 
-        else{
-            clearTimeout(typingTimer);
-            typingTimer = setTimeout(removeSelectionDrawer, typingThreshold)
-        }
-    })
-
 }
 function removeSelectionDrawer() {
     setTimeout(() => {
-        dropdownContainer.style.animation = "fadeOut ease 1.4s"
+        dropdownContainer.style.animation = "fadeOut ease 0.3s"
         dropdownContainer.classList.add("update")
-    }, 1400)
+    }, 300)
+    animationInitialise(dropdownContainer,"fadeOut ease 0.3s")
     parent.addEventListener("animationend", () => {
         parent.removeChild(dropdownContainer);
         isDrawer = false;
@@ -101,45 +82,9 @@ function removeSelectionDrawer() {
     })
 }
 
-function resolveElement(str) {
-    let rock = document.getElementById("rock");
-    let scissors = document.getElementById("scissors");
-    let paper = document.getElementById("paper");
-    str = str.toLocaleLowerCase()
-    console.log(str)
-    // match the text from the text area to the drop dropdown
-    //     starting letter
-    //     consecutive characters
-    //     lenght of chars
-    //     similarity with letters
-    //     the actual word but in diffrent case
-    if (str.length === 1) {
-        switch (str.charAt(0)) {
-            case 'r':
-                return rock;
-            case 's':
-                return scissors;
-            case 'p':
-                return paper;
-        }
-    } else if (str.length === 2) {
-        switch (str.charAt(1)) {
-            case 'o':
-                return rock;
-            case 'c':
-                return scissors;
-            case 'a':
-                return paper;
-        }
-    }
-}
-
-
-
 function runGame() {
     // get a random selection from rock,paper,scissors
     computerSelection = randomNumber % 3
-    // update the status bar
     let userSelection = options.indexOf(textarea.value.trim())
 
     let winScenarios = [ "0,2" , "1,0" , "2,1"] // (userSelection,computerSelection)
@@ -147,24 +92,33 @@ function runGame() {
     // paper = 1
     // scissors = 2
     if(winScenarios.includes(userSelection+","+computerSelection))
-        winGame();
+        concludeGame("you Win!","win")
     else if(userSelection === computerSelection)
-        tieGame();
+        concludeGame("it's a tie","tie")
     else 
-        loseGame();
-        // setup winGame and loseGame
-        // play an animation for the status bar
+        concludeGame("you Lose!","lose")
+    computerSelection = Math.floor(100* Math.random()) % 3
+    console.log("new selection: " + options[computerSelection])
+    textarea.value = ""
 }
+let animationTime = "0.7s"
+function concludeGame(statusString,animationName) {
+    console.log(statusString, textarea.value , options[computerSelection]) 
+    let animationStr = `${animationName} ease-in ${animationTime} 2 alternate`;
+    let header = document.getElementById("main-text")
+    
+    animationInitialise(header,animationStr);
+    
+    let status = document.getElementById("status")
+    status.innerText = statusString 
+    
+    animationInitialise(status,`${animationName}-status ease-in ${animationTime} 2 alternate`);
+}
+function animationInitialise(status,animationStr) {
+    status.style.animation = 'none';
+    status.style.animation = null;
+    setTimeout(() => { status.style.animation = animationStr });
+}
+startUp()
 
-function winGame() {
-    console.log("YOU WIN!")
-}
-
-function loseGame() {
-    console.log("You LOSE!", textarea.value , options[computerSelection])
-}
-function tieGame() {
-    console.log("You tie!")
-}
-
-startUp();
+setTimeout(winGame,2000)
